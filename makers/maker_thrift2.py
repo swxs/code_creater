75f14926ca60b08f92ab9b190b18aa508bdb78ec
swxs <466565029@qq.com>
2019-11-03 00:21:10 +0800

    [swxs] rpc

--- makers/maker_thrift2.py
+++ makers/maker_thrift2.py
@@ -4,6 +4,8 @@
 # @Time    : 2019/2/26 16:25
 
 import os
+
+from utils.utils import get_dir_path
 from .maker import Maker
 from .registry import factory
 
@@ -13,32 +15,35 @@ class MakerThrift2(Maker):
     name = "thrift2"
 
     def total_make(self, app_name, models, task):
+        dst_path = get_dir_path(task.get('target'), app_name, 'rpc')
+
         tmpl = os.path.join(task.get('framework'), 'protocols', 'model.thrift.jinja2')
-        dst_file = os.path.join(task.get('target'), app_name, 'protocols', 'model.thrift')
+        dst_file = os.path.join(dst_path, 'protocols', 'model.thrift')
         self.render(tmpl, {'models': models, 'app_name': app_name}, dst_file)
 
         tmpl = os.path.join(task.get('framework'), 'protocols', 'main.thrift.jinja2')
-        dst_file = os.path.join(task.get('target'), app_name, 'protocols', 'main.thrift')
+        dst_file = os.path.join(dst_path, 'protocols', 'main.thrift')
         self.render_once(tmpl, {'models': models, 'app_name': app_name}, dst_file)
 
         tmpl = os.path.join(task.get('framework'), '__init__.py.jinja2')
-        dst_file = os.path.join(task.get('target'), app_name, '__init__.py')
+        dst_file = os.path.join(dst_path, '__init__.py')
         self.render_once(tmpl, {'models': models, 'app_name': app_name}, dst_file)
 
         tmpl = os.path.join(task.get('framework'), 'base_client.py.jinja2')
-        dst_file = os.path.join(task.get('target'), app_name, 'base_client.py')
+        dst_file = os.path.join(dst_path, 'base_client.py')
         self.render(tmpl, {'models': models, 'app_name': app_name}, dst_file)
 
         tmpl = os.path.join(task.get('framework'), 'base_server.py.jinja2')
-        dst_file = os.path.join(task.get('target'), app_name, 'base_server.py')
+        dst_file = os.path.join(dst_path, 'base_server.py')
         self.render(tmpl, {'models': models, 'app_name': app_name}, dst_file)
 
         tmpl = os.path.join(task.get('framework'), 'client.py.jinja2')
-        dst_file = os.path.join(task.get('target'), app_name, 'client.py')
+        dst_file = os.path.join(dst_path, 'client.py')
         self.render_once(tmpl, {'models': models, 'app_name': app_name}, dst_file)
 
+
         tmpl = os.path.join(task.get('framework'), 'server.py.jinja2')
-        dst_file = os.path.join(task.get('target'), app_name, 'server.py')
+        dst_file = os.path.join(dst_path, 'server.py')
         self.render_once(tmpl, {'models': models, 'app_name': app_name}, dst_file)
 
     def make(self, app_name, model, task):
